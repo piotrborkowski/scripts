a1de2f1b90ffe8f4812fab599b63c2c505bfd9c3
piotrborkowski <boros@kki.pl>
2017-04-08 01:40:59 +0200

    Update upgrade_all.sh

--- upgrade_all.sh
+++ upgrade_all.sh
@@ -3,7 +3,7 @@ echo "Upgrading all packages using APT..."
 read -r -p "Do you want to continue? [y/N]" response
 case "$response" in
     [yY][eE][sS]|[yY])
-       	sudo apt-get --assume-yes update &&sudo apt-get --assume-yes upgrade &&sudo apt-get --assume-yes autoremove
+       	sudo /usr/bin/apt-get --assume-yes update &&sudo /usr/bin/apt-get --assume-yes upgrade &&sudo /usr/bin/apt-get --assume-yes autoremove
 	echo "Success!"
 	#exit
         ;;
